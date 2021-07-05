716e3549c354271ab195e223d6f53d93839781c2
Aniol Fernandez Cano <37707037+AniolFernandez@users.noreply.github.com>
2021-07-05 20:12:49 +0200

    Add files via upload

---
+++ Documentació/imports.sql
@@ -0,0 +1,20 @@
+INSERT INTO mmorpg.user_authority VALUES (1,'user');
+
+INSERT INTO mmorpg.class VALUES (1,'Swordsman');
+INSERT INTO mmorpg.class VALUES (2,'Archer');
+INSERT INTO mmorpg.class VALUES (3,'Magician');
+
+INSERT INTO mmorpg.npc VALUES (1, "Nailer", 5, 100, 100);
+
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-67.01, 5.69, 8.65, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-79.44, 7.96, 0.65, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-84.65, 9.20, -13.32, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-101.40, 16.51, -5.25, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-95.58, 16.54, 10.13, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (-92.06, 17.99, 19.82, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (30.43, 2.20, -23.43, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (23.97, 0.84, -39.72, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (37.00, 3.86, -46.19, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (54.33, 7.61, -35.09, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (48.87, 5.41, -11.57, 1);
+INSERT INTO mmorpg.npc_instance (position_x, position_y, position_z, NPC_npc_id) VALUES (35.72, 2.85, -10.07, 1);
